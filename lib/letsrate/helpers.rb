11fe7ca2ca3d9d084627ce0893b649da83a87c42
cubecul <peteh22@gmail.com>
2013-06-09 14:26:34 -0500

    Display individual ratings
    Create rating_for_user, which displays rating from an individual review rather than the overall rating.

--- lib/letsrate/helpers.rb
+++ lib/letsrate/helpers.rb
@@ -28,6 +28,26 @@ module Helpers
                 "data-readonly" => readonly,
                 "data-star-count" => star
   end
+  
+  def rating_for_user(rateable_obj, rating_user, dimension = nil, options = {})
+    @product = rateable_obj
+    @user = rating_user
+	  @rating = Rate.find_by_rater_id_and_rateable_id_and_dimension(@user.id, @product.id, dimension)
+	  stars = @rating.stars
+
+    disable_after_rate = options[:disable_after_rate] || false
+    
+    readonly=false
+    if disable_after_rate
+      readonly = current_user.present? ? !rateable_obj.can_rate?(current_user.id, dimension) : true
+    end
+ 
+    content_tag :div, '', "data-dimension" => dimension, :class => "star", "data-rating" => stars,
+                "data-id" => rateable_obj.id, "data-classname" => rateable_obj.class.name,
+                "data-disable-after-rate" => disable_after_rate,
+                "data-readonly" => readonly,
+                "data-star-count" => stars
+  end
 
 end
 
