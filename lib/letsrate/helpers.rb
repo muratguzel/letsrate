31c8d615b76b37ec5f3b7022c5e1244c22682c3e
Anton Orel <eagle.anton@gmail.com>
2013-07-14 22:49:19 +0400

    Reimplemented disable after rate option

--- lib/letsrate/helpers.rb
+++ lib/letsrate/helpers.rb
@@ -7,11 +7,9 @@ module Helpers
 
     star = options[:star] || 5
 
-    disable_after_rate = options[:disable_after_rate] || false
+    disable_after_rate = options[:disable_after_rate] || true
 
     readonly = !(current_user && rateable_obj.can_rate?(current_user, dimension))
-
-    disable_after_rate = current_user && disable_after_rate
 
     content_tag :div, '', "data-dimension" => dimension, :class => "star", "data-rating" => avg,
                 "data-id" => rateable_obj.id, "data-classname" => rateable_obj.class.name,
