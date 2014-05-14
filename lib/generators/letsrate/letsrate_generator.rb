39b7e3bc427e2949a3fddd720d5f14a7b6f2b837
James Naadjie <james.naadjie@gmail.com>
2014-05-13 20:14:31 -0400

    create_migration method def changed to create_letsrate_migration to avoid collision with rails create_migration method

--- lib/generators/letsrate/letsrate_generator.rb
+++ lib/generators/letsrate/letsrate_generator.rb
@@ -34,7 +34,7 @@ class LetsrateGenerator < ActiveRecord::Generators::Base
   end
 
   desc "migration is creating ..."
-  def create_migration
+  def create_letsrate_migration
     migration_template "migration.rb", "db/migrate/create_rates.rb"
   end
 end
